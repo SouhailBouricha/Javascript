491577c555c57fbee2d46a433f7789da8e738259
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-28 18:11:41 +0100

    16 Commmit

--- 08 - Website Project/app.js
+++ 08 - Website Project/app.js
@@ -87,11 +87,13 @@ function menuOpen(e){
     }
     console.log(e.target.children[1]);
 }
+const logo = document.querySelector('#logo');
 barba.init({
     views:[{
         namespace:'home',
         beforeEnter(){
             AnimationSlides();
+            logo.href = './index.html';
         },
         beforeLeave(){
             Controller.destroy();
@@ -100,6 +102,47 @@ barba.init({
         }
     },
     {
-        namespace:'fashion'
-    }]
+        namespace:'fashion',
+        beforeEnter(){
+            logo.href = '../index.html';
+        }
+    }],
+     transitions: [{
+        leave({ current, next }) {
+          let done = this.async();
+          //An Animation
+          const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+          tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+          tl.fromTo(
+            ".swipe",
+            0.75,
+            { x: "-100%" },
+            { x: "0%", onComplete: done },
+            "-=0.5"
+          );
+        },
+        enter({ current, next }) {
+          let done = this.async();
+          //Scroll to the top
+          window.scrollTo(0, 0);
+          //An Animation
+          const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+          tl.fromTo(
+            ".swipe",
+            1,
+            { x: "0%" },
+  
+            { x: "100%", stagger: 0.2, onComplete: done }
+          );
+          tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+          tl.fromTo(
+            ".nav-header",
+            1,
+            { y: "-100%" },
+            { y: "0%", ease: "power2.inOut" },
+            "-=1.5"
+          );
+        }
+      }
+    ]
 })
