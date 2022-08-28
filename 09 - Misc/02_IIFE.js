526eb4ade5e5f151f4ab312e405a97477f57c0ce
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-28 19:31:31 +0100

    18 Commit

---
+++ 09 - Misc/02_IIFE.js
@@ -0,0 +1,8 @@
+// :IIFE : An IIFE (Immediately Invoked Function Expression) is a 
+// JavaScript function that runs as soon as it is defined. 
+// The name IIFE is promoted by Ben Alman in his blog. (function () { // … })();
+
+
+(function Hello() {
+    console.log('ergerege');
+})();
