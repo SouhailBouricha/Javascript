f0b80538ab12a94af492d86ed5fde8673ea63f6f
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-29 21:21:46 +0100

    25 Commit

--- 10 - Async JavaScript/04_Promises.js
+++ 10 - Async JavaScript/04_Promises.js
@@ -1,6 +1,6 @@
 /* The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
 
-To learn about the way promises workand how you can use them, we advise you to read Using promises first.*/
+To learn about the way promises work and how you can use them, we advise you to read Using promises first.*/
 
 // const promise = new Promise((resolve,reject) =>{
 //     setTimeout(() =>{
