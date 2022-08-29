42b64a594fb06a2318ac8b971132967317a52735
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-29 18:06:35 +0100

    20 Commit

---
+++ 10 - Async JavaScript/01_AJAX.js
@@ -0,0 +1,8 @@
+// 01 Ajax : Asynchronous JavaScript and XML, while not a technology in itself, is a term coined in 2005 by 
+// Jesse James Garrett, that describes a "new" approach to using a number of existing technologies together, 
+// including HTML or XHTML, CSS, JavaScript, DOM, XML, XSLT, and most importantly the XMLHttpRequest object. 
+// When these technologies are combined in the Ajax model, web applications are able to make quick, incremental
+//  updates to the user interface without reloading the entire browser page. This makes the application faster 
+//  and more responsive to user actions. Although X in Ajax stands for XML, JSON is preferred over XML nowadays
+//   because of its many advantages such as being a part of JavaScript, 
+// thus being lighter in size. Both JSON and XML are used for packaging information in the Ajax model.
