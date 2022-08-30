17737dbc303e09eb213a242e263f173ef3bdcb9d
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-30 18:11:21 +0100

    27 Commit

---
+++ 10 - Async JavaScript/Async_JavaScript_Part_3.js
@@ -0,0 +1,16 @@
+function getData(type,url,async,callback){
+    const xhr = new XMLHttpRequest();
+    xhr.open(type,url,async);
+    xhr.onload = () => {        
+    if(xhr.status == 200){
+        callback(xhr.response);
+    }
+}
+    xhr.send();
+}
+
+function ShowData(data){
+    console.log(data);
+}
+
+getData("GET","https://restcountries.com/v3.1/all",true,ShowData);
