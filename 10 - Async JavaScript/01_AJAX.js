55333856d9c748ba2923e40dd313b5d334cbecdb
souhail bouricha <60862274+SouhailBouricha@users.noreply.github.com>
2022-08-31 19:47:03 +0100

    31 Commit

--- 10 - Async JavaScript/01_AJAX.js
+++ 10 - Async JavaScript/01_AJAX.js
@@ -41,11 +41,24 @@
 //   });
 
 // console.log("Started request…");
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+// fetchPromise.then((response) => {
+//   const jsonPromise = response.json();
+//   jsonPromise.then((data) => {
+//     console.log(data[0].name);
+//   });
+// });
+
 const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 
-fetchPromise.then((response) => {
-  const jsonPromise = response.json();
-  jsonPromise.then((data) => {
+fetchPromise
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
     console.log(data[0].name);
   });
-});
