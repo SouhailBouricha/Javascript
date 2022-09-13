a39f984bedb921cc0d2c5848cdb16bc828160430
souhail bouricha <souhailbourigha@gmail.com>
2022-09-13 23:40:13 +0100

    dds

--- 12 - dds/file.js
+++ 12 - dds/file.js
@@ -97,3 +97,20 @@ const FactoryFunction = string => {
   const jimmie = Player('jim', 10);
   const badGuy = Player('jeff', 5);
   jimmie.attack(badGuy);
+
+//   const Person = (name) => {
+//     const sayName = () => console.log(`my name is ${name}`);
+//     return {sayName};
+//   }
+  
+  const Nerd = (name) => {
+    // simply create a person and pull out the sayName function with destructuring assignment syntax!
+    const {sayName} = Person(name);
+    const doSomethingNerdy = () => console.log('nerd stuff');
+    return {sayName, doSomethingNerdy};
+  }
+  
+//   const jeff = Nerd('jeff');
+  
+  jeff.sayName(); //my name is jeff
+  jeff.doSomethingNerdy(); // nerd stuff
