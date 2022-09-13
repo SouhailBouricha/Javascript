3b90ccfa3ba7b805c1338ede452a0bc8e607104a
souhail bouricha <souhailbourigha@gmail.com>
2022-09-13 23:39:00 +0100

    dds

--- 12 - dds/file.js
+++ 12 - dds/file.js
@@ -40,3 +40,15 @@ const func = x => {
 func(99);
 
 console.log(a); // ???????
+const FactoryFunction = string => {
+    const capitalizeString = () => string.toUpperCase();
+    const printString = () => console.log(`----${capitalizeString()}----`);
+    return { printString };
+  };
+  
+  const taco = FactoryFunction('taco');
+  
+  printString(); // ERROR!!
+  capitalizeString(); // ERROR!!
+  taco.capitalizeString(); // ERROR!!
+  taco.printString(); // this prints "----TACO----"
