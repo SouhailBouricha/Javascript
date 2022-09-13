1b2b91ca03a80797337530bbd25f2f7ace4364e3
souhail bouricha <souhailbourigha@gmail.com>
2022-09-13 23:36:54 +0100

    dds

--- 12 - dds/file.js
+++ 12 - dds/file.js
@@ -3,8 +3,16 @@ const personFactory = (name, age) => {
     return { name, age, sayHello };
   };
   
-  const jeff = personFactory('jeff', 27);
+//   const jeff = personFactory('jeff', 27);
   
   console.log(jeff.name); // 'jeff'
   
   jeff.sayHello(); // calls the function and logs 'hello!'
+
+  const Person = function(name, age) {
+    this.sayHello = () => console.log('hello!');
+    this.name = name;
+    this.age = age;
+  };
+  
+  const jeff = new Person('jeff', 27);
