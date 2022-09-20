2e027ac00e8106c5dfac61abe16a5fedbd2db527
souhail bouricha <souhailbourigha@gmail.com>
2022-09-20 23:34:33 +0100

    Class

--- 12 - dds/Classes.js
+++ 12 - dds/Classes.js
@@ -67,3 +67,9 @@ console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 var name = "Bob", time = "today";  
 console.log(`Hello ${name}, how are you ${time}?`);
+
+import moment from 'moment';
+console.log("Hello from JavaScript!");  
+console.log(moment().startOf('day').fromNow());
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, how are you ${time}?`);
