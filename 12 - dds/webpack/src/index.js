ed692b0c103f0d67d01d50a1173c1471d014ed68
souhail bouricha <souhailbourigha@gmail.com>
2022-09-25 14:54:27 +0100

    webpack

--- 12 - dds/webpack/src/index.js
+++ 12 - dds/webpack/src/index.js
@@ -1,10 +1,11 @@
-import _ from 'lodash';
+import myName from './myName';
+
 function component() {
     const element = document.createElement('div');
 
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack']);
+    element.innerHTML = myName("Souhail");
   
     return element;
   }
