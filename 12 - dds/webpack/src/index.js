b2622d71bd69175ac423ac43933be6cbf5f26d66
souhail bouricha <souhailbourigha@gmail.com>
2022-09-25 18:32:20 +0100

    webpack

--- 12 - dds/webpack/src/index.js
+++ 12 - dds/webpack/src/index.js
@@ -1,6 +1,21 @@
 import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
@@ -13,8 +28,10 @@ function component() {
     // Add the image to our existing div.
     const myIcon = new Image();
     myIcon.src = Icon;
-    console.log(Icon);
     element.appendChild(myIcon);
+    
+    console.log(Data);
+    console.log(Notes);
 
     return element;
   }
