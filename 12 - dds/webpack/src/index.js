b2744c4e6fd9653ce55815e314f6d7ad8d067fe2
souhail bouricha <souhailbourigha@gmail.com>
2022-09-25 18:16:17 +0100

    webpack

--- 12 - dds/webpack/src/index.js
+++ 12 - dds/webpack/src/index.js
@@ -1,5 +1,6 @@
 import myName from './myName';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = myName("Souhail");
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    console.log(Icon);
+    element.appendChild(myIcon);
 
     return element;
   }
